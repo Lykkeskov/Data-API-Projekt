08923302f5d7960ec48cf079ec6dd7759a78c0fd
Tobias-Henriksen <160583454+Tobias-Henriksen@users.noreply.github.com>
2025-09-19 11:40:25 +0200

    Add files via upload

--- InformatikLysSensor.ino
+++ InformatikLysSensor.ino
@@ -1,18 +1,37 @@
 //Brug WEMOS D1 MINI ESP32
-// Pins for lys sensor 1 og 2
-const int  LysSensorPin1 = 34;
+// Definer pins der bruges
+const int lysSensorPin1 = 34;
+const int buttonPin = 19;
+const int ledPin = 18;
+
+const int delayTime = 1000;
+unsigned long previousMillis = 0;
+
+bool ledState = LOW;
 
 void setup() {
   // Sæt sensorer til at være inputs
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
 
+  digitalWrite(ledPin, ledState);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print("Sensor 1:");
-  Serial.println(analogRead(LysSensorPin1));
+  int buttonState = digitalRead(buttonPin);
+  unsigned long currentMillis = millis();
+  
+  if (buttonState == LOW && currentMillis - previousMillis >= delayTime) {
+    previousMillis = currentMillis;
+    
+    Serial.print("Sensor 1:");
+    Serial.println(analogRead(lysSensorPin1));
+    
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+  }
 
-delay(50);
 
 }
