2c1df6c61ec305cf405cc95299fb316762a38938
Tobias-Henriksen <160583454+Tobias-Henriksen@users.noreply.github.com>
2025-10-29 12:37:13 +0100

    Updated arduino program

--- InformatikLysSensor2.ino
+++ InformatikLysSensor2.ino
@@ -1,59 +1,94 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const int lysSensorPin1 = 34;
+// --- Pins ---
 const int buttonPin = 19;
 const int ledPin = 18;
- 
+
+// --- Timing ---
 const int delayTime = 3000;
 unsigned long previousMillis = 0;
-
 bool ledState = LOW;
 
+// --- WiFi ---
 const char* ssid = "Tobias_iPhone";
 const char* password = "iPhoneNet";
-const char* server = "https://halfdan.pythonanywhere.com/data";
+const char* server = "http://halfdan.pythonanywhere.com/data";
 
 void setup() {
   Serial.begin(115200);
+  
+  // Setup pins
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+
+  // Connect to WiFi
+  Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting...");
+    Serial.print(".");
   }
-  Serial.println("Connected to WiFi");
-  Serial.print("IP is ");
+
+  Serial.println("\nConnected to WiFi");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
-  // Sæt sensorer til at være inputs
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT);
-
 }
 
 void loop() {
   int buttonState = digitalRead(buttonPin);
   unsigned long currentMillis = millis();
 
+  // Trigger when button pressed and delay time passed
   if (buttonState == LOW && currentMillis - previousMillis >= delayTime) {
-    previousMillis = currentMillis;    
+    previousMillis = currentMillis;
 
+    // Toggle LED
     ledState = !ledState;
     digitalWrite(ledPin, ledState);
 
-    if (WiFi.status() == WL_CONNECTED) {
-      HTTPClient http;
-      http.begin(server);
-      http.addHeader("Content-Type", "application/json");
+    // Read light level
+    int lysniveau = analogRead(34);
+    int lokale = 211;
 
-      String json = "{\"lokale\":211, \"lysniveau\":67}";
-      int httpResponseCode = http.POST(json);
+    sendData(lokale, lysniveau);
+  }
+}
 
-      Serial.print("Response: ");
+void sendData(int lokale, int lysniveau) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(server);
+    http.addHeader("Content-Type", "application/json");
+
+    // Build JSON payload
+    String json = "{\"lokale\":";
+    json += lokale;
+    json += ",\"lysniveau\":";
+    json += lysniveau;
+    json += "}";
+
+    Serial.print("Sending data: ");
+    Serial.println(json);
+
+    int httpResponseCode = http.POST(json);
+
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.print("Server response: ");
+      Serial.println(response);
+    } else {
+      Serial.print("Error code: ");
       Serial.println(httpResponseCode);
-
-      http.end();
     }
+
+    http.end();
+  } else {
+    Serial.println("WiFi not connected. Reconnecting...");
+    WiFi.reconnect();
   }
 }
